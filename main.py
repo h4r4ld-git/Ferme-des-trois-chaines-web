437448b77dd39ab888f35770c4f0c6aee53e8c31
hrld hrld <luka.chkoidze@student.uclouvain.be>
2021-05-02 05:46:12 +0200

    Creation de figures

--- main.py
+++ main.py
@@ -1,10 +1,42 @@
 import sqlite3
+from flask import Flask, render_template
 
-conn = sqlite3.connect('Vaches.db')
+app = Flask(__name__)
+@app.route('/')
 
-cursor = conn.cursor()
+def index():
+    """
+    retourne le contenu de la page index.html
+    """
+    conn = sqlite3.connect('Vaches.db')
+    cursor = conn.cursor()
+    mnth = []
+    for row in cursor.execute("SELECT date from velages"):
+        if row[0].split("/")[1:] == ["10","2020"]:
+            mnth.append(row[0])
 
-for row in cursor.execute("SELECT id, famille_id, sexe, presence, apprivoise, mort_ne, decede from animaux"):
-    print(row[0], "\t", row[1], "\t", row[2], "\t", row[3], "\t", row[4], "\t", row[5])
-    break
-conn.close()
+    freq = []
+    for i in range(1,29):
+        date = str(i) + "/10/2020"
+        if i < 10:
+            date = "0" + date
+
+        if date in mnth:
+            freq.append(mnth.count(date))
+        else:
+            freq.append(0)
+
+    mort_nids = []
+    freq1 = [0 for i in range(12)]
+    for row in cursor.execute("SELECT id from animaux where mort_ne=1"):
+        mort_nids.append(row[0])
+    for i in mort_nids:
+        for row in cursor.execute(f"SELECT date from velages where id={i}"):
+            if row[0].split("/")[2] == "2020":
+                freq1[int(row[0].split("/")[1])-1] += 1
+
+    conn.close()
+    return render_template("index.html", jours = [i for i in range(1,29)], freq = freq, freq1 = freq1)
+
+if __name__ == "__main__":
+    app.run()
