9396bac924a8c003d474c2ca34b89a27ec7a20f8
hrld hrld <luka.chkoidze@student.uclouvain.be>
2021-05-03 23:45:03 +0200

    Amélioration databaser_heritage

--- sqlfiles/databaser_heritage.py
+++ sqlfiles/databaser_heritage.py
@@ -9,14 +9,11 @@ def heritage_genetique(parent):
     Pre : 'parent' est un id d'un animale et se trouve dans la base de données
     Post : Retourne un dictionnaire en format {'id de la race' : 'pourcentage'}
     """
-    a_types = [] # Les ID des animaux deja présents dans la table animaux_types
-    for row in cursor.execute("SELECT animal_id from animaux_types"):
-        a_types.append(row[0])
-    if parent in a_types: # Si l'animal est présent dans la table animaux_types
-        types = {} # Dictionnaire en format {'id du type' : 'pourcentage'}
-        for row in cursor.execute(f"SELECT type_id, pourcentage from animaux_types where animal_id={parent}"):
-            types[int(row[0])] = float(row[1])
+
+    if parent in [i[0] for i in a_types]: # Si l'animal est présent dans la table animaux_types
+        types = {int(row[0]) : float(row[1]) for row in cursor.execute(f"SELECT type_id, pourcentage from animaux_types where animal_id={parent}")} # Dictionnaire en format {'id du type' : 'pourcentage'}
         return types
+
     for row in cursor.execute(f'SELECT velages.id, velages.mere_id, velages.pere_id, animaux_velages.animal_id, animaux_velages.velage_id from animaux_velages, velages where animaux_velages.animal_id="{parent}" and velages.id=animaux_velages.velage_id'):
         type1 = heritage_genetique(row[1]) # Races de la Mere
         type2 = heritage_genetique(row[2]) # Races du Pere
@@ -36,18 +33,16 @@ def heritage_genetique(parent):
                 retl[i] = (t1 + t2)/2
         return retl
 
-ids = [] # ID des animaux
-for row in cursor.execute("SELECT id from animaux"):
-    ids.append(row[0])
+ids = [row[0] for row in cursor.execute("SELECT id from animaux")] # ID des animaux
+
+a_types = [[row[0], row[1]] for row in cursor.execute("SELECT animal_id, type_id from animaux_types")] # Liste en format [['id de l'animal', 'id du type']]
 
 for i in ids: # Pour chaque animal
-    a_types = [] # Liste en format [['id de l'animal', 'id du type']]
-    for row in cursor.execute("SELECT animal_id, type_id from animaux_types"):
-        a_types.append([row[0],row[1]])
     her = heritage_genetique(i) # Heritage genetique de l'animal
     for j in her.items(): # Pour chaque type et pourcentage
         if [i,j[0]] not in a_types: # Si id de l'animal et du type n'est pas présent dans la table animaux_types
             cursor.execute(f"INSERT INTO animaux_types (animal_id, type_id, pourcentage) VALUES ({int(i)},{int(j[0])},{float(j[1])});") # Inserer dans animaux_types les types et pourcentage de l'animal
+            a_types.append([i,j[0]])
 conn.commit() # Enregistrer
 
 conn.close() # Terminer la connection
