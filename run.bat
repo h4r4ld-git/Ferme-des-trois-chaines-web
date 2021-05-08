1235ac3fff6d4c78f14cdd79c2255cacd94dc90b
hrld hrld <luka.chkoidze@student.uclouvain.be>
2021-05-08 15:55:35 +0200

    RUN files

---
+++ run.bat
@@ -0,0 +1,4 @@
+CALL venv\Scripts\activate
+set FLASK_APP=flaskr
+set FLASK_ENV=development
+flask run
---
+++ run.sh
@@ -0,0 +1,5 @@
+. ./venv/bin/activate
+export FLASK_APP=flaskr
+export FLASK_ENV=development
+flask run
+deactivate
